da4dd2cc7d321bf4b48de6733edb2cd972a38ba0
Mehmed Selimovic <mehmed.selimovic.ets@gmail.com>
2023-01-25 20:41:54 +0100

    Registration page & base routing of login and register buttons

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 "use strict";
 
 const loginBtn = document.getElementById("login");
+const registerBtn = document.getElementById("register");
 
+// Routing
 loginBtn.addEventListener("click", () => {
-  window.open("login.html", "_blank");
-  console.log("klik");
+  window.open("login.html", "_self");
+});
+
+registerBtn.addEventListener("click", () => {
+  window.open("register.html", "_self");
 });
